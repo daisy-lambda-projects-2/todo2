bf0c430f93471a2a9a8e8174d18caa6ce2e113fd
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-12-03 19:20:46 -0500

    Updating service worker

--- public/worker.js
+++ public/worker.js
@@ -19,19 +19,62 @@ self.addEventListener('install', event => {
 });
 
 // Cache and return requests
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(function (response) {
+//                 // Cache hit - return response
+//                 if (response) {
+//                     return response;
+//                 }
+//                 return fetch(event.request);
+//             }
+//             )
+//     );
+// });
+
+
+// ============ NEW ===============
+// On fetch, use cache but update the entry with the latest contents from the server.
+self.addEventListener('fetch', e => {
+    e.respondWith(fromCache(e.request));
+    e.waitUntil(
+        update(e.request)
+            .then(refresh)
+    )
+})
+
+const fromCache = request => {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match(request);
+    })
+}
+
+const update = request => {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response.clone().then(function () {
+                return response;
+            }))
+        })
+    })
+}
+
+function refresh(response) {
+    return self.clients.matchAll().then(function (clients) {
+        clients.forEach(function (client) {
+            var message = {
+                type: 'refresh',
+                url: response.url,
+                eTag: response.headers.get('ETag')
             }
-            )
-    );
-});
+            client.postMessage(JSON.stringify(message))
+        })
+    })
+}
+
+// ===========================
+
 
 // Update a service worker
 self.addEventListener('activate', event => {
